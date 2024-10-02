49452f6aad1fe90a7c17c4f7a5a3904d69b9319a
mohammedshanan <mohammedshana12@gmail.com>
2024-10-03 00:27:49 +0300

    added docs

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -19,11 +19,12 @@ def pascal_triangle(n):
     if n < 1:
         return []
     while i < n:
-        array = []
-        for index in range(i + 1):
-            prev = triangle[i - 1][index - 1] if index > 0 else 0
-            current = triangle[i - 1][index] if index != len(triangle[i - 1]) else 0
-            array.append(prev + current)
+        cur_row = []
+        prev_row = triangle[i - 1]
+        for j in range(i + 1):
+            prev = prev_row[j - 1] if j > 0 else 0
+            cur = prev_row[j] if j != len(prev_row) else 0
+            cur_row.append(prev + cur)
         i += 1
-        triangle.append(array)
+        triangle.append(cur_row)
     return triangle
