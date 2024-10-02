0785b77d43fc6031b52b36c80e2ac9afc51385d9
mohammedshanan <mohammedshana12@gmail.com>
2024-10-03 00:10:48 +0300

    added docs

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,19 @@
+#!/usr/bin/python3
+"""A module for Pascal's triangle"""
+
+
 def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to a given number of rows.
+
+    Args:
+        n (int): The number of rows of Pascal's Triangle to generate.
+
+    Returns:
+        list of list of int: A list of lists where each inner list
+        represents a row in Pascal's Triangle.
+        If the input is not a positive integer, an empty list is returned.
+    """
     triangle = [[1]]
     i = 1
     if n <= 1:
